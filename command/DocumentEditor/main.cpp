450dc5d46ee45d497f6396cfb1abd5089de1ce30
sergeythrees <sergeythrees@gmail.com>
2017-03-29 02:48:20 +0300

    правки в названии и структуре проекта

--- command/DocumentEditor/main.cpp
+++ command/DocumentEditor/main.cpp
@@ -1,125 +1,100 @@
-#include <string>
-#include <iostream>
-#include <memory>
-#include <boost/optional.hpp>
-#include <boost/filesystem.hpp>
+п»ї// ISpringWord.cpp : Defines the entry point for the console application.
+//
 
+#include "stdafx.h"
+#include "Menu.h"
+#include "Document.h"
 
 using namespace std;
+using namespace std::placeholders;
 
-typedef boost::filesystem::path Path;
-using boost::optional;
-using boost::none;
+namespace
+{
 
-class IParagraph;
-class IImage;
-
-/*
-Неизменяемый элемент документа
-*/
-class CConstDocumentItem
+class CEditor
 {
 public:
-	// Возвращает указатель на константное изображение, либо nullptr, если элемент не является изображением
-	shared_ptr<const IImage> GetImage()const;
-	// Возвращает указатель на константный параграф, либо nullptr, если элемент не является параграфом
-	shared_ptr<const IParagraph> GetParagraph()const;
-	virtual ~CConstDocumentItem() = default;
+	CEditor()  //-V730
+		:m_document(make_unique<CDocument>())
+	{
+		m_menu.AddItem("help", "Help", [this](istream&) { m_menu.ShowInstructions(); });
+		m_menu.AddItem("exit", "Exit", [this](istream&) { m_menu.Exit(); });
+		AddMenuItem("setTitle", "Changes title. Args: <new title>", &CEditor::SetTitle);
+		m_menu.AddItem("list", "Show document", bind(&CEditor::List, this, _1));
+		AddMenuItem("undo", "Undo command", &CEditor::Undo);
+		AddMenuItem("redo", "Redo undone command", &CEditor::Redo);
+	}
+
+	void Start()
+	{
+		m_menu.Run();
+	}
+
+private:
+	// РЈРєР°Р·Р°С‚РµР»СЊ РЅР° РјРµС‚РѕРґ РєР»Р°СЃСЃР° CEditor, РїСЂРёРЅРёРјР°СЋС‰РёР№ istream& Рё РІРѕР·РІСЂР°С‰Р°СЋС‰РёР№ void
+	typedef void (CEditor::*MenuHandler)(istream & in);
+
+	void AddMenuItem(const string & shortcut, const string & description, MenuHandler handler)
+	{
+		m_menu.AddItem(shortcut, description, bind(handler, this, _1));
+	}
+
+	// TODO: СЃРєРёРїРЅСѓС‚СЊ РїРµСЂРІС‹Р№ РїСЂРѕР±РµР» СЌР»РµРіР°РЅС‚РЅРµРµ
+	void SetTitle(istream & in)
+	{
+		string head;
+		string tail;
+
+		if (in >> head)
+		{
+			getline(in, tail);
+		}
+		string title = head + tail;
+
+		m_document->SetTitle(title);
+	}
+
+	void List(istream &)
+	{
+		cout << "-------------" << endl;
+		cout << m_document->GetTitle() << endl;
+		cout << "-------------" << endl;
+	}
+
+	void Undo(istream &)
+	{
+		if (m_document->CanUndo())
+		{
+			m_document->Undo();
+		}
+		else
+		{
+			cout << "Can't undo" << endl;
+		}
+	}
+
+	void Redo(istream &)
+	{
+		if (m_document->CanRedo())
+		{
+			m_document->Redo();
+		}
+		else
+		{
+			cout << "Can't redo" << endl;
+		}
+	}
+
+	CMenu m_menu;
+	unique_ptr<IDocument> m_document;
 };
 
-/*
-Элемент документа. Позволяет получить доступ к изображению или параграфу
-*/
-class CDocumentItem : public CConstDocumentItem
-{
-public:
-	// Возвращает указатель на изображение, либо nullptr, если элемент не является изображением
-	shared_ptr<IImage> GetImage();
-	// Возвращает указатель на параграф, либо nullptr, если элемент не является параграфом
-	shared_ptr<IParagraph> GetParagraph();
-};
-
-/* Параграф текста*/
-class IParagraph
-{
-public:
-	virtual string GetText()const = 0;
-	virtual void SetText(const string& text) = 0;
-	virtual ~IParagraph() = default;
-};
-
-/*
-Изображение
-*/
-class IImage
-{
-public:
-	// Возвращает путь относительно каталога документа
-	virtual Path GetPath()const = 0;
-
-	// Ширина изображения в пикселях
-	virtual int GetWidth()const = 0;
-	// Высота изображения в пикселях
-	virtual int GetHeight()const = 0;
-	
-	// Изменяет размер изображения
-	virtual void Resize(int width, int height) = 0;
-	
-	virtual ~IImage() = default;
-};
-
-/*
-Интерфес документа
-*/
-class IDocument
-{
-public:
-	// Вставляет параграф текста в указанную позицию (сдвигая последующие элементы)
-	// Если параметр position не указан, вставка происходит в конец документа
-	virtual shared_ptr<IParagraph> InsertParagraph(const string& text, 
-		optional<size_t> position = none) = 0;
-
-	// Вставляет изображение в указанную позицию (сдвигая последующие элементы)
-	// Параметр path задает путь к вставляемому изображению
-	// При вставке изображение должно копироваться в подкаталог images 
-	// под автоматически сгенерированным именем
-	virtual shared_ptr<IImage> InsertImage(const Path& path, int width, int height, 
-		optional<size_t> position = none) = 0;
-	
-	// Возвращает количество элементов в документе
-	virtual size_t GetItemsCount()const = 0;
-
-	// Доступ к элементам изображения
-	virtual CConstDocumentItem GetItem(size_t index)const = 0;
-	virtual CDocumentItem GetItem(size_t index) = 0;
-
-	// Удаляет элемент из документа
-	virtual void DeleteItem(size_t index) = 0;
-
-	// Возвращает заголовок документа
-	virtual string GetTitle()const = 0;
-	// Изменяет заголовок документа
-	virtual void SetTitle(const string & title) = 0;
-
-	// Сообщает о доступности операции Undo
-	virtual bool CanUndo()const = 0;
-	// Отменяет команду редактирования
-	virtual void Undo() = 0;
-
-	// Сообщает о доступности операции Redo
-	virtual bool CanRedo()const = 0;
-	// Выполняет отмененную команду редактирования
-	virtual void Redo() = 0;
-
-	// Сохраняет документ в формате html. Изображения сохраняются в подкаталог images
-	// пути к изображениям указываются относительно пути к сохраняемому HTML файлу
-	virtual void Save(const Path& path)const = 0;
-
-	virtual ~IDocument() = default;
-};
-
+}
 
 int main()
 {
-
+	CEditor editor;
+	editor.Start();
+	return 0;
 }
+
--- command/command/ICommand.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-class ICommand
-{
-public:
-	virtual void Execute() = 0;
-	virtual ~ICommand() = default;
-};
