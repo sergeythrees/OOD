ce358e54d318edf17cf8f7f50fa83c0bb2c467ae
sergeythrees <sergeythrees@gmail.com>
2017-03-29 03:40:15 +0300

    добавил каталог для тестов

---
+++ 05_command/DocumentEditor_tests/VerifyExceptions.h
@@ -0,0 +1,12 @@
+#pragma once
+namespace
+{
+	template <typename Ex, typename Fn>
+	void VerifyException(Fn && fn, const std::string & expectedDescription)
+	{
+		BOOST_CHECK_EXCEPTION(fn(), Ex, [&](const Ex& e) {
+			BOOST_CHECK_EQUAL(e.what(), expectedDescription);
+			return e.what() == expectedDescription;
+		});
+	}
+}
